9d958d9812bab7aa2eac1b165a003f31d569c967
laisiewcheng <laisiewcheng@hotmail.com>
2020-04-24 12:54:18 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -95,6 +95,7 @@ if (args.testOnly):
 
     if use_cuda:
         net.cuda()
+        print('Lets use ', torch.cuda.device_count(), ' GPUs')
         net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
         cudnn.benchmark = True
 
